887c27efc892700442f06ff12d187e062da5e351
Jason Gresh <jrgresh@gmail.com>
2016-09-16 16:58:19 -0400

    Fix button disable bug

--- TabbedMonty/NCardMontyViewController.swift
+++ TabbedMonty/NCardMontyViewController.swift
@@ -17,33 +17,33 @@ class NCardMontyViewController: UIViewController {
     let howManyCards: Int
     
     let brain: MontyBrain
-    
+    var loaded: Bool
     let resetTitle = "Reset"
     
     required init?(coder aDecoder: NSCoder) {
         self.howManyCards = 19
+        self.loaded = false
         self.brain = MontyBrain(numCards: self.howManyCards)
         super.init(coder: aDecoder)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
     override func viewDidLayoutSubviews() {
-                setUpGameButtons(v: buttonContainer, totalButtons: self.howManyCards, buttonsPerRow: 6)
-        self.view.setNeedsDisplay()
+        if !loaded {
+            setUpGameButtons(v: buttonContainer, totalButtons: self.howManyCards, buttonsPerRow: 6)
+            self.view.setNeedsDisplay()
+        }
+        loaded = true
     }
     
     func resetButtonColors() {
-        for v in view.subviews {
+        for v in buttonContainer.subviews {
             if let button = v as? UIButton {
-                if button.currentTitle != resetTitle {
-                    button.backgroundColor = UIColor.blue
-                    button.isEnabled = true
-                    
-                }
+                button.backgroundColor = UIColor.blue
+                button.isEnabled = true
             }
         }
     }
@@ -54,11 +54,9 @@ class NCardMontyViewController: UIViewController {
     }
     
     func disableCardButtons() {
-        for v in view.subviews {
+        for v in buttonContainer.subviews {
             if let button = v as? UIButton {
-                if button.currentTitle != resetTitle {
-                    button.isEnabled = false
-                }
+                button.isEnabled = false
             }
         }
     }
